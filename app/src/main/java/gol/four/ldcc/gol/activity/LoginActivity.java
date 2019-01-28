6369d91edc465810a12169d629acd20422bd9045
songmilee <lsm941010@gmail.com>
2019-01-28 11:16:04 +0900

    add all android file

---
+++ app/src/main/java/gol/four/ldcc/gol/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package gol.four.ldcc.gol.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import gol.four.ldcc.gol.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
