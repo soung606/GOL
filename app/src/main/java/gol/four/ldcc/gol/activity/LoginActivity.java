619987e6ed5e32d073f73685509517e71de5704c
Lissena-Hwang <ghkdguswjd1225@gmail.com>
2019-01-29 16:26:26 +0900

    nfc send message add

--- app/src/main/java/gol/four/ldcc/gol/activity/LoginActivity.java
+++ app/src/main/java/gol/four/ldcc/gol/activity/LoginActivity.java
@@ -4,19 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-<<<<<<< Updated upstream
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
-=======
->>>>>>> Stashed changes
 
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import java.util.ArrayList;
@@ -56,11 +52,11 @@ public class LoginActivity extends AppCompatActivity {
                     result.enqueue(new Callback<ArrayList<JsonObject>>() {
                         @Override
                         public void onResponse(Call<ArrayList<JsonObject>> call, Response<ArrayList<JsonObject>> response) {
-                            //Log.d("TEST!!", response.body().toString());
                             //[{"model":"GoL_Server.employee","pk":2,"fields":{"login_id":"qw","password":"12","name":"test","dt_register":"2019-01-29T00:52:48.998Z","authority":0,"auth":0}}]
 
                             JsonObject user_fields = (JsonObject) response.body().get(0).get("fields");
-                            String auth = user_fields.get("auth").toString();
+                            String auth = user_fields.get("authority").toString();
+                            String pk = response.body().get(0).get("pk").toString();
                             if(auth.equals("1")){
                                 Intent intent=new Intent(LoginActivity.this,AdminMenuActivity.class);
                                 startActivity(intent);
@@ -69,22 +65,27 @@ public class LoginActivity extends AppCompatActivity {
                                 Intent intent=new Intent(LoginActivity.this,WorkerMenuActivity.class);
                                 startActivity(intent);
                             }
+
+                            SharedPreferences sf = getSharedPreferences(sfName, 0);
+                            SharedPreferences.Editor editor = sf.edit();//저장하려면 editor가 필요
+                            editor.putString("id", id); // 아이디 입력
+                            editor.putString("auth", auth);//권한저장
+                            editor.putString("pk", pk);
+
                             if(auto_login.isChecked() == true){
-                                SharedPreferences sf = getSharedPreferences(sfName, 0);
-                                SharedPreferences.Editor editor = sf.edit();//저장하려면 editor가 필요
-                                editor.putString("id", id); // 아이디 입력
                                 editor.putString("password", password);// 비번입력
-                                editor.putString("auth", auth);//권한저장
                                 editor.putBoolean("isFirst", true);//자동로그인 저장
-                                editor.commit(); // 파일에 최종 반영함
 
                             }
-
+                            else{
+                                editor.putBoolean("isFirst", false);//자동로그인 안 저장
+                            }
+                            editor.commit();// 파일에 최종 반영함
                             finish();
                         }
                         @Override
                         public void onFailure(Call<ArrayList<JsonObject>> call, Throwable t) {
-                            Log.d("TEST", t.getMessage());
+                            Snackbar.make(binding.autoLogin,"아이디와 비밀번호를 다시 확인해 주세요",Snackbar.LENGTH_SHORT);
                         }
                     });
                 }
